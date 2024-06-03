a3a67ee4d2499a3c2e789e1566d02f5f79f69274
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2024-06-03 14:49:25 +0200

    Replaces SpacedQMer.h to include the new method

--- src/Spaced/SpacedQmer.cpp
+++ src/Spaced/SpacedQmer.cpp
@@ -1,7 +1,5 @@
 #include "SpacedQmer.h"
 #include <algorithm>
-#include <coin/CbcModel.hpp>
-#include <coin/OsiClpSolverInterface.hpp>
 
 SpacedQmer::SpacedQmer(const std::vector<std::string>& seedPatterns) : seedPatterns(seedPatterns) {
     parseSeedPatterns();
@@ -21,57 +19,6 @@ std::vector<int> SpacedQmer::parseSinglePattern(const std::string& pattern) cons
         }
     }
     return pos;
-}
-
-std::set<int> SpacedQmer::linearProgrammingSetCovering(const std::string& sequence) const {
-    int numSeeds = seedPatterns.size();
-    int seqLen = sequence.size();
-
-    OsiClpSolverInterface solver;
-    CoinModel model;
-
-    // Create variables for each seed
-    for (int i = 0; i < numSeeds; ++i) {
-        model.setColumnBounds(i, 0, 1);
-        model.setObjectiveCoefficient(i, 1);
-    }
-
-    // Map positions to seeds
-    std::map<int, std::vector<int>> positionToSeeds;
-    for (int i = 0; i < numSeeds; ++i) {
-        for (int pos : positions[i]) {
-            if (pos < seqLen) {
-                positionToSeeds[pos].push_back(i);
-            }
-        }
-    }
-
-    // Add constraints to ensure each position is covered
-    for (const auto& [pos, seeds] : positionToSeeds) {
-        CoinPackedVector constraint;
-        for (int seed : seeds) {
-            constraint.insert(seed);
-        }
-        model.addRow(constraint, 1.0, COIN_DBL_MAX);
-    }
-
-    // Load problem to solver
-    solver.loadFromCoinModel(model);
-
-    // Create and solve the model
-    CbcModel cbcModel(solver);
-    cbcModel.solve();
-
-    // Extract the selected seeds
-    std::set<int> selectedSeeds;
-    const double* solution = cbcModel.bestSolution();
-    for (int i = 0; i < numSeeds; ++i) {
-        if (solution[i] > 0.5) {
-            selectedSeeds.insert(i);
-        }
-    }
-
-    return selectedSeeds;
 }
 
 std::vector<std::string> SpacedQmer::generateSpacedQmers(const std::string& sequence) const {
--- src/Spaced/SpacedQmer.h
+++ src/Spaced/SpacedQmer.h
@@ -1,25 +1,55 @@
-#ifndef SPACEDQMER_H
-#define SPACEDQMER_H
+#ifndef SRC_HASH_SPACEDQMER_H_
+#define SRC_HASH_SPACEDQMER_H_
 
-#include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include <coin/CbcModel.hpp>
-#include <coin/OsiClpSolverInterface.hpp>
+#include <string>
+
+using namespace std;
+
+typedef vector<size_t> Position;
+typedef struct {
+    vector<size_t> one_to_remove;
+    vector<size_t> one_to_keep;
+    vector<size_t> one_to_change;
+} PreviousShift;
+typedef vector<PreviousShift> V_PreviusShift;
+typedef vector<V_PreviusShift> V_V_PreviusShift;
 
 class SpacedQmer {
 public:
-    SpacedQmer(const std::vector<std::string>& seedPatterns);
-    std::vector<std::string> generateSpacedQmers(const std::string& sequence) const;
+    SpacedQmer();
+    SpacedQmer(string spaced_qmer, size_t numprev);
+    void reset(string spaced_qmer, size_t numprev);
+    void SaveIndexOne();
+    void GetShiftMax(V_PreviusShift& shift_max);
+    void SetAllMultipleShift();
+    void SetBitMasks();
+    
+    // New method for set covering
+    void SetCoveringLP();
+
+    inline bool isOne(size_t index) const { return this->spaced_q[index] == '1'; }
+    inline const Position& GetPosOne() const { return this->pos_one; }
+    inline const V_PreviusShift& GetShiftMinChange() const { return this->shift_min_change; }
+    inline const V_V_PreviusShift& GetMultipleShifts() const { return this->multiple_shifts; }
+    inline const V_V_PreviusShift* GetMultipleShiftsPointer() const { return &this->multiple_shifts; }
+    inline const string& toString() const { return this->spaced_q; }
 
 private:
-    std::vector<std::string> seedPatterns;
-    std::vector<std::vector<int>> positions;
+    string spaced_q;
+    Position pos_one;
+    Position pos_pos_one;
+    V_PreviusShift shift_min_change;
+    V_V_PreviusShift multiple_shifts;
+    size_t num_prev = 0;
 
-    void parseSeedPatterns();
-    std::vector<int> parseSinglePattern(const std::string& pattern) const;
-    std::set<int> linearProgrammingSetCovering(const std::string& sequence) const;
+    void SetMultipleShifts(size_t index);
 };
 
-#endif // SPACEDQMER_H
+// Support functions
+void printp(Position p);
+void print_shift(PreviousShift s);
+void deleteElement(Position& pos, size_t index);
+bool isContained(Position pointer, Position pos, size_t index);
+
+#endif /* SRC_HASH_SPACEDQMER_H_ */
